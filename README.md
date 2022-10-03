d0ce78fd8acd1844046b6127b4d2f75184ecacc3
Andrii Zadvornyi <azadvornyi@deloitte.com>
2022-10-03 20:20:28 +0300

    (init) init commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Practical lesson stt-pz-5
+
+> In this lesson is introduction to End2End testing in javascript. Using Cypress
+> Cypress is a next generation front end testing tool built for the modern web. We address the key pain points developers and QA engineers face when testing modern applications.
+
+### What need to do:
+* Setting up the testing environment
+* Implement e2e tests for cypress/e2e/calculator.spec.cy.js 
+
+### Acceptance criteria
+* Understand basic concepts of End2End test in JavaScript
+* Setting up the testing environment
+* Build and run them
+
+### Getting started
+
+Install npm packages
+
+```
+npm install
+```
+Start App
+
+```
+npm run dev
+```
+
+Run tests e2e
+
+```
+npm run cypress:open
+```
+
+# Useful links
+* [ Introduction to End-to-End testing with Cypress](https://wanago.io/2019/12/30/javascript-testing-introduction-end-to-end-testing-cypress/).
+* [Why Cypress?](https://docs.cypress.io/guides/overview/why-cypress).
+* [Пирамида тестирования на практике.](https://dou.ua/lenta/columns/testing-in-jiji/).
+* [Пирамида тестирования](https://tlroadmap.io/roles/technical-lead/product-quality/testing/test-pyramid.html)
+
+
+
---
+++ app/libs/calculator.js
@@ -0,0 +1,60 @@
+const actions = ['+', '-', '*', '/', '.', '%'];
+
+const dashboard = document.getElementById("dashboard");
+
+function printAction(val) {
+  if (val === '+/-') {
+    let firstDigit = dashboard.value[0]
+    if (firstDigit === '-') {
+      dashboard.value = dashboard.value.slice(1, dashboard.value.length)
+    } else {
+      dashboard.value = '-' + dashboard.value
+    }
+  } else if (actions.includes(dashboard.value[dashboard.value.length - 1])
+    || dashboard.value.length === 0) {
+  } else {
+    dashboard.value += val
+  }
+}
+
+function print(val) {
+  dashboard.value += val
+}
+
+function solve() {
+  let expression = dashboard.value
+  dashboard.value = math.evaluate(expression)
+}
+
+function clr() {
+  dashboard.value = ''
+}
+
+function setTheme(themeName) {
+  localStorage.setItem('theme', themeName);
+  document.querySelector('body').className = themeName;
+}
+
+function toggleTheme() {
+  let theme = localStorage.getItem('theme');
+
+  if (theme === 'theme-second') {
+    theme = 'theme-one'
+  } else if (theme === 'theme-one') {
+    theme = 'theme-second'
+  }
+  setTimeout(() => {
+    setTheme(theme);
+  }, 500)
+}
+
+function save() {
+  localStorage.setItem('result', dashboard.value);
+}
+
+function paste() {
+  print(localStorage.getItem('result'))
+}
+
+
+setTheme('theme-one');
