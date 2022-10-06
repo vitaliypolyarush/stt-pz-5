ec44e1bd9d35f1f111527deb925eab88572c64cf
Andrii Zadvornyi <azadvornyi@deloitte.com>
2022-10-06 17:48:07 +0300

    (fix) fix bug with defined variables

--- cypress/e2e/calculator.spec.cy.js
+++ cypress/e2e/calculator.spec.cy.js
@@ -4,14 +4,30 @@ describe('e2e test for calculator app', () => {
 
   before(() => {
     cy.visit('http://localhost:3000/');
-    bnt1 = cy.get('[data-test="btn-1"]');
-    btn2 = cy.get('[data-test="btn-2"]');
-    btn6 = cy.get('[data-test="btn-6"]');
-    btn9 = cy.get('[data-test="btn-9"]');
-    btnPlus = cy.get('[data-test="btn-plus"]');
-    btnMinus = cy.get('[data-test="btn-minus"]');
-    dashboard = cy.get('[data-test="dashboard"]');
-    btnEqual = cy.get('[data-test="btn-equal"]');
+    cy.get('[data-test="btn-1"]').then(($el) => {
+      bnt1 = $el
+    });
+    btn2 = cy.get('[data-test="btn-2"]').then(($el) => {
+      btn2 = $el
+    });
+    cy.get('[data-test="btn-6"]').then(($el) => {
+      btn6 = $el
+    });
+    cy.get('[data-test="btn-9"]').then(($el) => {
+      btn9 = $el
+    });
+    cy.get('[data-test="btn-plus"]').then(($el) => {
+      btnPlus = $el
+    });
+    cy.get('[data-test="btn-minus"]').then(($el) => {
+      btnMinus = $el
+    });
+    cy.get('[data-test="dashboard"]').then(($el) => {
+      dashboard = $el
+    });
+    cy.get('[data-test="btn-equal"]').then(($el) => {
+      btnEqual = $el
+    });
   });
 
   it('should check expression 2+9-6', () => {
@@ -21,7 +37,7 @@ describe('e2e test for calculator app', () => {
     btnMinus.click()
     btn6.click()
 
-    dashboard.invoke('val').then(val => {
+    cy.wrap(dashboard).invoke('val').then(val => {
       expect(val).to.equal('2+9-6');
     })
 
@@ -29,44 +45,44 @@ describe('e2e test for calculator app', () => {
 
   it('should check result of expression 2+9-6', () => {
     btnEqual.click()
-    dashboard.invoke('val').then(val => {
+    cy.wrap(dashboard).invoke('val').then(val => {
       expect(val).to.equal('5');
     })
 
   })
   it('should check result of expression 1+2+3+4-5-6-7-8-9', () => {
-    // Write your code here
+    throw new Error("Write your code here")
   })
   it('should check result expression 10/10', () => {
-    // Write your code here
+    throw new Error("Write your code here")
   })
 
   it('should check result expression 0.5*10', () => {
-    // Write your code here
+    throw new Error("Write your code here")
   })
 
   it('should check result expression -0.1*99', () => {
-    // Write your code here
+    throw new Error("Write your code here")
   })
 
   it('should calculate 10 percent of 1,000', () => {
-    // Write your code here
+    throw new Error("Write your code here")
   })
 
   it('should check clear button', () => {
-    // Write your code here
+    throw new Error("Write your code here")
   })
 
   it('should check save button', () => {
-    // Write your code here
+    throw new Error("Write your code here")
   })
 
   it('should check paste button', () => {
-    // Write your code here
+    throw new Error("Write your code here")
   })
 
   it('should check change theme, toggle theme button', () => {
-    // Write your code here
+    throw new Error("Write your code here")
   })
 
 })
